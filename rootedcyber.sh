d1657adda897ae83bd18f2aff86e48cd2549ea4e
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-06-06 20:04:54 +0530

    Update rootedcyber.sh

--- rootedcyber.sh
+++ rootedcyber.sh
@@ -1,7 +1,7 @@
 apt update
 apt upgrade
 apt install git python3 python3-pip -y
-git clone 
+git clone https://github.com/my-fork-repo/WZML
 cd WZ*
 pip3 install --no-cache-dir -r requirements.txt
 bash start.sh
